8b2e73b0ecbf38ce6e9bec2c4be317398b020fad
= <gonzru01atwork@gmail.com>
2022-10-28 13:28:38 -0400

    better navbar

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 //hiding navbar
 let prevScrollPos = window.scrollY;
+const navListItem = document.getElementsByClassName("nav-list-item");
+const navlist = document.querySelector(".nav-list");
+const hamburguerBtn = document.getElementById("hmbgrBtn");
 
 window.onscroll = () => {
     let navBar = document.querySelector('.nav-bar');
@@ -8,14 +11,23 @@ window.onscroll = () => {
         navBar.style.top = "0";
     } else {
         navBar.style.top = "-130px"
+        if (navlist.classList[0] === "nav-list-active") {
+            navlist.classList = "nav-list";
+        }
     }
     prevScrollPos = currentScrollPos;
 }
 
-let navListItem = document.getElementsByClassName("nav-list-item");
 
-let navlist = document.querySelector(".nav-list");
 
+hamburguerBtn.addEventListener("click", () => {
+    if (navlist.classList[0] === "nav-list-active") {
+        navlist.classList = "nav-list";
+    } else {
+        navlist.classList = "nav-list-active";
+    }
+    
+});
 
 window.onload = () => {
     navlist.style.gridTemplateColumns = "1fr"
